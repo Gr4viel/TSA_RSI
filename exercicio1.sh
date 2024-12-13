cc7542cc66efb7de27f8eff60c7cefbcd2fe70ca
Rafa-0002 <rafafaustino18@hotmail.com>
2024-12-13 15:49:19 +0000

    Adicionar nome e disciplinas curso

---
+++ exercicio1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#script que pergunta nome do curso, quantidade de disciplinas e nomes das disciplinas
+# E adiciona tudo a um ficheiro txt
+
+arquivo="disciplinas.txt"
+
+> $arquivo
+
+echo "Qual é o nome do curso que está a frequentar?"
+read curso
+
+echo "Curso: $curso" >> $arquivo
+echo "Disciplinas do curso $curso:" >> $arquivo
+
+echo "Quantas disciplinas quer adicionar?"
+read quantidade
+
+for ((i=1; i<=quantidade; i++)) # Loop para repetir a pergunta o número de vezes que colocamos em cima.
