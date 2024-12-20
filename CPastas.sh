b5205068f37dbdc1118bff77f44ad410f523c25d
Gr4viel <antonio2004810@gmail.com>
2024-12-20 14:12:32 +0000

    CPastas v2

--- CPastas.sh
+++ CPastas.sh
@@ -9,3 +9,12 @@ fi
 
 mkdir -p disciplinas
 
+tail -n +3 "$ficheiro" | while IFS= read -r disciplina # lê o ficheiro linha a linha apartir da terceira linha
+do
+  if [[ -n "$disciplina" ]]; then # verifica quando acaba o ficheiro
+    mkdir -p "disciplinas/$disciplina"
+    echo "Pasta criada para a disciplina: $disciplina"
+  fi
+done
+
+echo "Pastas para as disciplinas foram criadas."
